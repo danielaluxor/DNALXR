1d971b941eac26f552c974528d5c8f3e260850fd
danielaluxor <daniela.luxor13@gmail.com>
2026-05-21 23:20:43 -0600

    Update initDB.js

--- core/system/initDB.js
+++ core/system/initDB.js
@@ -8,14 +8,14 @@ function initDB(m, client) {
   settings.prefix ??= ['/', '!', '.', '#']
   settings.commandsejecut ??= isNumber(settings.commandsejecut) ? settings.commandsejecut : 0
   settings.id ??= '120363401404146384@newsletter'
-  settings.nameid ??= "'ೃ࿔ ყµҡเ ωαɓσƭ'ร - σƒƒเ૮เαℓ ૮ɦαɳɳεℓ .ೃ࿐"
+  settings.nameid ??= "'ೃ࿔ Dana Lxr .ೃ࿐"
   settings.type ??= 'Owner'
   settings.link ??= 'https://api.yuki-wabot.my.id'
   settings.banner ??= 'https://cdn.yuki-wabot.my.id/files/tCVD.jpeg'
   settings.icon ??= 'https://cdn.yuki-wabot.my.id/files/PJDp.jpeg'
   settings.currency ??= 'Yenes'
-  settings.namebot ??= 'Yuki'
-  settings.botname ??= 'Yuki Suou'  
+  settings.namebot ??= 'Dana'
+  settings.botname ??= 'DnlxrBot'  
   settings.owner ??= ''
 
   const user = global.db.data.users[m.sender] ||= {}
