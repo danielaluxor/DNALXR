5892eca05cd1aa7df8a10585d2a0310b0aa7a49b
danielaluxor <daniela.luxor13@gmail.com>
2026-05-21 23:29:11 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,21 +2,21 @@ import fs from 'fs';
 import { watchFile, unwatchFile } from 'fs'
 import { fileURLToPath } from 'url'
 
-global.owner = ['573196588149', '5492916450307', '5216671548329', '573247662531', '51921826291', '50493732693']
+global.owner = ['573196588149', '5492916450307', '5216671548329', '573247662531', '51921826291', '50493732693','5219932099478']
 global.botNumber = ''
 
 global.sessionName = 'Sessions/Owner'
 global.version = '^2.0 - Latest'
-global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜"
+global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ Dana"
 global.links = {
 api: 'https://api.yuki-wabot.my.id',
-channel: "https://whatsapp.com/channel/0029Vb64nWqLo4hb8cuxe23n",
+channel: "https:https://whatsapp.com/channel/0029VbDOzTyLI8YRPPVlBI1l",
 github: "https://github.com/iamDestroy/YukiBot-MD",
 gmail: "thekingdestroy507@gmail.com"
 }
 global.my = {
 ch: '120363401404146384@newsletter',
-name: 'ೃ࿔ ყµҡเ ωαɓσƭร - σƒƒเ૮เαℓ ૮ɦαɳɳεℓ .ೃ࿐',
+name: 'ೃ࿔ Danabot.ೃ࿐',
 }
 
 global.mess = {
